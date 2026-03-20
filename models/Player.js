aadd915aa4e09465345d486d4947b7134af7205d
vukicmihajlo13-sketch <vukicmihajlo13@gmail.com>
2026-03-20 19:43:27 +0100

    Update Player.js

--- models/Player.js
+++ models/Player.js
@@ -1,10 +1,14 @@
 import mongoose from 'mongoose';
 
 const PlayerSchema = new mongoose.Schema({
-  ign: { type: String, required: true, unique: true },
-  tier: { type: String, default: 'LT5' },
-  region: { type: String, default: 'NA' },
-  points: { type: Number, default: 0 }, // This is your ELO
-}, { timestamps: true });
+  ign: { type: String, required: true },
+  discordId: { type: String, required: true, unique: true },
+  tier: { type: String, required: true },
+  region: { type: String, required: true },
+  avatar: { type: String },
+  // Crucial: Default to 0 so they don't show up on the site automatically
+  points: { type: Number, default: 0 }, 
+  updatedAt: { type: Date, default: Date.now }
+});
 
 export default mongoose.models.Player || mongoose.model('Player', PlayerSchema);
